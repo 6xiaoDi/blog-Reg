60092604864840484fce93365c43533b5ed761f7
6xd <ldtheended@163.com>
2020-10-29 16:46:25 +0800

    a0.54（():分组—— 命名分组）

--- 24..命名分组.js
+++ 24..命名分组.js
@@ -1,4 +1,6 @@
 let str = "fdsabc123fdsfdsabc444";
-let reg = /(abc)(\d+)/;
-let res =  str.match(reg);
+// let reg = /(abc)(\d+)/;
+// let res =  str.match(reg);
+let reg = /(abc)(?<num>\d+)/;
+let res =  reg.exec(str);
 console.log(res);
