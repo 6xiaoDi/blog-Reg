3ef94adf19434ed8b2cd94dd0cde35e29fdc6656
6xd <ldtheended@163.com>
2020-10-29 16:51:14 +0800

    a0.55（():分组—— 不捕获分组）

--- 24..命名分组.js
+++ 24..命名分组.js
@@ -1,6 +1,7 @@
 let str = "fdsabc123fdsfdsabc444";
 // let reg = /(abc)(\d+)/;
 // let res =  str.match(reg);
-let reg = /(abc)(?<num>\d+)/;
+// let reg = /(abc)(?<num>\d+)/;
+let reg = /(?:abc)(?<num>\d+)/;
 let res =  reg.exec(str);
 console.log(res);
