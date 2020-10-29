bc15313367bb9dd8f90364b04de8f273b323c15b
6xd <ldtheended@163.com>
2020-10-29 15:32:33 +0800

    a0.38（字符集合——连接符的使用3）

--- 19.字符集合.js
+++ 19.字符集合.js
@@ -1,5 +1,6 @@
 let str = "fdsafdSAaf4324abABCf4535dsaf";
 // let reg = /[a-z]/g;
-let reg = /[a-zA-Z]/g
+// let reg = /[a-zA-Z]/g
+let reg = /[0-9]/g
 let res =  str.replace(reg,"*");
 console.log(res);
