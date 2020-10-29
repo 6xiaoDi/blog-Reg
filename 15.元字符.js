3902143221da0c4c106c8683b5c07354783dbf60
6xd <ldtheended@163.com>
2020-10-29 14:41:14 +0800

    a0.30（元字符  =>  `.` 和` *`使用2）

--- 15.元字符.js
+++ 15.元字符.js
@@ -1,4 +1,6 @@
-let str = `<div>fdsafdsaf</div>`;
+// let str = `<div>fdsafdsaf</div>`;
+let str = `<div>fdsa
+fdsaf</div>`;
 // 注意</div>中“/”需要转义，否则判定为正则的结束符号了！
 let reg = /<div>.*<\/div>/g;
 let res =  reg.test(str);
