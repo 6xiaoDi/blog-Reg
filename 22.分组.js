0448f0b516933a5e386b2d1baa5405dd4eea95bc
6xd <ldtheended@163.com>
2020-10-29 16:26:38 +0800

    a0.48（():分组—— 取值）

--- 22.分组.js
+++ 22.分组.js
@@ -1,4 +1,7 @@
 let str = "fdsabc123fdsfdsabc444";
-let reg = /abc\d+/g;
+// let reg = /abc\d+/g;
+let reg = /(abc)(\d+)/g; // 分组取数据
 let res =  str.match(reg);
-console.log(res);
+// console.log(res);
+let $1 = RegExp.$1;
+console.log(RegExp.$1);
