a14776fad477fb0b75edfc0c9fd01c7c126f7418
6xd <ldtheended@163.com>
2020-10-29 15:41:30 +0800

    a0.42（数量词汇——指定`a`出现`3`次）

--- 20.数量词汇.js
+++ 20.数量词汇.js
@@ -1,6 +1,7 @@
 // {}数量；
 let str = "abcaaabcdaafdsa";
 // let reg= /aa/g;
-let reg= /a{2}/g; // 指定a出现两次
+// let reg= /a{2}/g; // 指定a出现两次
+let reg= /a{3}/g; // 指定a出现3次
 let res =  str.replace(reg,"*");
 console.log(res);
