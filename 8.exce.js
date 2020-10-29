add4dc7366ebb8b9ed805591b3e16b6f6dbde0b7
6xd <ldtheended@163.com>
2020-10-29 13:43:35 +0800

    a0.15（正则方法 => test 、exce ——exce使用2）

--- 8.exce.js
+++ 8.exce.js
@@ -1,4 +1,7 @@
 let reg = /\d+/g;
 let str = "fdasf12fdaf121";
-let res =  reg.exec(str)
-console.log(res);
+// let res =  reg.exec(str)
+// console.log(res);
+console.log(reg.exec(str));
+console.log(reg.exec(str));
+console.log(reg.exec(str));
