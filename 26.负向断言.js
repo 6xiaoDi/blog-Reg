5f49372daf3d25d4ddc55972a695b97e2efbc3e1
6xd <ldtheended@163.com>
2020-10-29 19:21:03 +0800

    a0.61（():分组——负向否定断言）

--- 26.负向断言.js
+++ 26.负向断言.js
@@ -1,5 +1,6 @@
 let str = "10px20px30pxipx";  //px 转成 像素；
 // let reg = /\d{2}px/g;
-let reg = /(?<=\d{2})px/g;
+// let reg = /(?<=\d{2})px/g;
+let reg = /(?<!\d{2})px/g;
 let res = str.replace(reg,"像素");
 console.log(res);
