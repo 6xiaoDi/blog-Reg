18fd81478646aedc3dfa815699d1b43d2e20356b
6xd <ldtheended@163.com>
2020-10-29 15:34:54 +0800

    a0.39（字符集合——匹配0~20失败）

--- 19.字符集合.js
+++ 19.字符集合.js
@@ -1,6 +1,7 @@
 let str = "fdsafdSAaf4324abABCf4535dsaf";
 // let reg = /[a-z]/g;
 // let reg = /[a-zA-Z]/g
-let reg = /[0-9]/g
+// let reg = /[0-9]/g
+let reg = /[0-20]/g
 let res =  str.replace(reg,"*");
 console.log(res);
