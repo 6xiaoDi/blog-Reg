05f93283416803b1c1027572259998dbfc8cb13c
6xd <ldtheended@163.com>
2020-10-29 19:11:11 +0800

    a0.58（():分组——正向否定断言）

--- 25.正向肯定断言.js
+++ 25.正向肯定断言.js
@@ -2,6 +2,7 @@ let str = "iphone6iphone7iphone11iphonenumber";
 // iphone-->苹果
 // let reg = /iphone\d{1,2}/g
 // ?= 表示可能存在后面的内容
-let reg = /iphone(?=\d{1,2})/g
+// let reg = /iphone(?=\d{1,2})/g
+let reg = /iphone(?!\d{1,2})/g // =换成！
 let res  = str.replace(reg,"苹果");
 console.log(res);
