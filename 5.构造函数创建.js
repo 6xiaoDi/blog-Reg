49ccdd39a14a276312cb3d9e31cac009d6642fa2
6xd <ldtheended@163.com>
2020-10-29 13:00:43 +0800

    a0.09（构造函数创建全局匹配正则）

--- 5.构造函数创建.js
+++ 5.构造函数创建.js
@@ -1,5 +1,6 @@
 let mystr = "hello";
 let str = "hello world1231";
-let reg = new RegExp(mystr);
+// let reg = new RegExp(mystr)
+let reg = new RegExp(mystr,"g");;
 let res  = str.match(reg);
 console.log(res);
