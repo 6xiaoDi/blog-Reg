2131ea86cd5b7796343a686bd8172b7630b32256
6xd <ldtheended@163.com>
2020-10-29 19:28:59 +0800

    a0.64（匹配模式——默认非粘性模式）

---
+++ 28.粘性模式.js
@@ -0,0 +1,8 @@
+let str = "fda244dfa12sf";
+let reg =  /\d/g;
+console.log(reg.exec(str));
+console.log(reg.exec(str));
+console.log(reg.exec(str));
+console.log(reg.exec(str));
+console.log(reg.exec(str));
+console.log(reg.exec(str));
