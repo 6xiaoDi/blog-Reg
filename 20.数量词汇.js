b1ad6ddfc7ec0c1082d21cfb48a4e346f0a0e47f
6xd <ldtheended@163.com>
2020-10-29 15:44:40 +0800

    a0.44（数量词汇——指定`a`出现`1`次或`3`次）

--- 20.数量词汇.js
+++ 20.数量词汇.js
@@ -3,6 +3,7 @@ let str = "abcaaabcdaafdsa";
 // let reg= /aa/g;
 // let reg= /a{2}/g; // 指定a出现两次
 // let reg= /a{3}/g; // 指定a出现3次
-let reg= /a{2,3}/g; // 指定a出现2次或3次
+// let reg= /a{2,3}/g; // 指定a出现2次或3次
+let reg= /a{1,3}/g; // 指定1次到3次
 let res =  str.replace(reg,"*");
 console.log(res);
